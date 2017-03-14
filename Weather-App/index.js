c4cc6a7b3218fbc588cf79990f36c23fe94402c4
Charles Sexton <charlie_sexton@yahoo.com>
2017-03-13 21:10:45 -0400

    Added icons to the weather-app, includes ajax call....

--- Weather-App/index.js
+++ Weather-App/index.js
@@ -1,5 +1,46 @@
-navigator.geolocation.getCurrentPosition(function(location) {
-    console.log(location.coords.latitude);
-    console.log(location.coords.longitude);
-    console.log(location.coords.accuracy);
-});
+window.onload = function() {
+
+    navigator.geolocation.getCurrentPosition(function(location) {
+        console.log(location.coords.latitude);
+        console.log(location.coords.longitude);
+        console.log(location.coords.accuracy);
+
+        weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+            location.coords.latitude + '&lon=' + location.coords.longitude +
+            "&APPID=c91312d00d5875753efe8c433770ee39";
+
+        loadJSON(weatherUrl,
+            function(data) {
+                console.log(data.main.temp);
+                console.log(data.main.humidity);
+                console.log(data.weather[0].description);
+                console.log(data);
+                document.getElementsByTagName("body")[0].className = "";
+            },
+            function(xhr) {
+                console.error(xhr);
+            }
+        );
+
+    });
+
+};
+
+
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                success(JSON.parse(xhr.responseText));
+            } else {
+                error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
