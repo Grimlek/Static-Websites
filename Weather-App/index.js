b7556a139b0d02e0d88f326881540395f836a64f
Charles Sexton <charlie_sexton@yahoo.com>
2017-06-10 23:12:02 -0400

    Finished static website weather app.

--- Weather-App/index.js
+++ Weather-App/index.js
@@ -1,3 +1,5 @@
+var kelvinTemperature;
+
 window.onload = function() {
 
     navigator.geolocation.getCurrentPosition(function(location) {
@@ -11,42 +13,75 @@ window.onload = function() {
 
         loadJSON(weatherUrl,
             function(data) {
-                document.getElementsByTagName("section")[0].className = "";
-                document.getElementsByClassName("weather-info-wrapper")[0].className = "";
-                document.getElementsByClassName("weather-info").className = "";
-
-
-
-                console.log(data.main.temp);
-                console.log(convertKelvinToFarenheit(data.main.temp));
-
-
-
                 iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
                 document.getElementsByTagName("img")[0].src = iconUrl;
 
-                temperatureFarenheit = convertKelvinToFarenheit(data.main.temp);
-                document.getElementsByClassName("info-temp")[0].innerText = temperatureFarenheit;
+                kelvinTemperature = data.main.temp;
+                temperatureFarenheit = convertKelvinToFarenheit(kelvinTemperature);
+                document.getElementsByClassName("info-temp")[0].innerText = temperatureFarenheit + "\u2109";
 
+                document.getElementsByClassName("info-location")[0].innerText = data.name;
 
+                weather = data.weather[0].description;
+                document.getElementsByClassName("info-description")[0].innerText = weather;
+                setBackgroundImage(weather);
 
-                console.log(data.main.humidity);
-                console.log(data.weather[0].description);
-                console.log(data);
+                windText = degreeToWindDirection(data.wind.deg) + " " + data.wind.speed + " knots";
+                document.getElementsByClassName("info-wind")[0].innerText = windText;
+
+                document.getElementsByClassName("loading")[0].style.display = "none";
+                document.getElementsByClassName("container")[0].style.display = "block";
             },
             function(xhr) {
-                document.getElementsByTagName("section")[0].className = "";
-                var infoWrapper = document.getElementsByClassName("weather-info-wrapper")[0];
+                document.getElementsByClassName("loading")[0].style.display = "none";
+                document.getElementsByClassName("container")[0].style.display = "block";
+
+                infoWrapper = document.getElementsByClassName("weather-info-wrapper")[0];
                 infoWrapper.className = "error";
                 infoWrapper.innerText = "An error has occurred loading your local weather!";
             }
         );
     });
+
+
+    document.getElementsByClassName("temp-toggle")[0].onclick = function () {
+        var temperatureElement = document.getElementsByClassName("info-temp")[0];
+        if (temperatureElement.innerText.indexOf("\u2103") != -1) {
+            temperatureFarenheit = convertKelvinToFarenheit(kelvinTemperature);
+            temperatureElement.innerText = temperatureFarenheit + "\u2109";
+        }
+        else {
+            temperatureCelcius = convertKelvinToCelcius(kelvinTemperature);
+            temperatureElement.innerText = temperatureCelcius + "\u2103";
+        }
+    };
 };
 
 
 function convertKelvinToFarenheit(temp) {
     return Math.round((temp - 273.15) * 1.8 + 32);
+}
+
+
+function convertKelvinToCelcius(temp) {
+    return Math.round(kelvinTemperature - 273.15)
+}
+
+
+function degreeToWindDirection(degree) {
+    var directionNames = [ "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" ]
+    var value = Math.floor(degree/22.5 + .5);
+    return directionNames[value % 16]
+}
+
+
+function setBackgroundImage(weather) {
+    if (weather == "scattered clouds") {
+        document.body.style.background = "url('images/cloudy.png') no-repeat center center";
+    }
+    else if (weather == "clear sky" && temperatureFarenheit > 60) {
+        document.body.style.background = "url('images/warm.png') no-repeat center center";
+    }
 }
 
 
